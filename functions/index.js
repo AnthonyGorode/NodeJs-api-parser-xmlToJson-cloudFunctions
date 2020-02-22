5ab382173896e3f116ec34b72baf9433a2fa2a84
Anthony Gorode <anthony.gorode@gmail.com>
2020-02-22 15:36:24 +0100

    Started cloud functions with firebase

---
+++ functions/index.js
@@ -0,0 +1,34 @@
+const functions = require('firebase-functions');
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const feedRoutes = require("./routes/feeds.routing");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use((req,res,next) => {
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATH, PUT, DELETE, OPTIONS"
+    );
+    next();
+});
+
+app.use("/api/feed",feedRoutes);
+
+app.use("/",(req,res,next) => {
+    res.status(200).json({
+        message: "Welcome to my first api for to parse xml feed to json"
+    });
+});
+
+exports.app = functions.https.onRequest(app);
